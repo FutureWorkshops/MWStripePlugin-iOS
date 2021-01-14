90658e022601dcec4734e7277785a54a110b244a
Xavi Moll <xmollv@gmail.com>
2021-01-14 15:20:13 +0100

    Added the correct AppDelegate/SceneDelegate

--- MWStripe/MWStripe/AppDelegate.swift
+++ MWStripe/MWStripe/AppDelegate.swift
@@ -6,16 +6,18 @@
 //
 
 import UIKit
+import MobileWorkflowCore
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-
+class AppDelegate: UIResponder, UIApplicationDelegate, AppEventDelegator {
+    
+    weak var eventDelegate: AppEventDelegate?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
-
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
 }
-
