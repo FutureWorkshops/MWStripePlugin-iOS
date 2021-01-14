bff8bd5cbe20a649c0eed84c57285c0160275eba
Xavi Moll <xmollv@gmail.com>
2021-01-14 15:17:30 +0100

    Added the Plugin project

---
+++ MWStripePlugin/MWStripePlugin/MWStripePlugin.h
@@ -0,0 +1,18 @@
+//
+//  MWStripePlugin.h
+//  MWStripePlugin
+//
+//  Created by Xavi Moll on 14/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MWStripePlugin.
+FOUNDATION_EXPORT double MWStripePluginVersionNumber;
+
+//! Project version string for MWStripePlugin.
+FOUNDATION_EXPORT const unsigned char MWStripePluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MWStripePlugin/PublicHeader.h>
+
+
