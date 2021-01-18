0628b9a3e8252c289bd0b216a37bb2a76efb3083
Xavi Moll <xmollv@gmail.com>
2021-01-18 13:00:24 +0100

    Implemented Step2 of the process by conforming to STPCustomerEphemeralKeyProvider

--- MWStripePlugin/MWStripePlugin/StripePlugin/MWStripeViewController.swift
+++ MWStripePlugin/MWStripePlugin/StripePlugin/MWStripeViewController.swift
@@ -17,7 +17,48 @@ public class MWStripeViewController: ORKStepViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Step 1 - Provide a publishable key to set up the SDK
         StripeAPI.defaultPublishableKey = self.stripeStep.publishableKey
     }
     
+}
+
+// MARK: Stripe API
+enum MWStripeError: LocalizedError {
+    case failedToConstructEphemeralURL
+    
+    var errorDescription: String? {
+        switch self {
+        case .failedToConstructEphemeralURL: return "Failed to construct the URL to retrieve the ephemeral key."
+        }
+    }
+}
+
+// Step 2 - Set up an ephemeral key: https://stripe.com/docs/mobile/ios/basic#ephemeral-key
+extension MWStripeViewController: STPCustomerEphemeralKeyProvider {
+    public func createCustomerKey(withAPIVersion apiVersion: String, completion: @escaping STPJSONResponseCompletionBlock) {
+        guard var urlComponents = URLComponents(url: self.stripeStep.ephemeralKeyURL, resolvingAgainstBaseURL: false) else {
+            completion(nil, MWStripeError.failedToConstructEphemeralURL)
+            return
+        }
+        
+        urlComponents.queryItems = [URLQueryItem(name: "api_version", value: apiVersion)]
+        
+        guard let finalURL = urlComponents.url else {
+            completion(nil, MWStripeError.failedToConstructEphemeralURL)
+            return
+        }
+        
+        var request = URLRequest(url: finalURL)
+        request.httpMethod = "POST"
+        
+        let task = URLSession.shared.dataTask(with: request) { dataOrNil, urlResponseOrNil, errorOrNil in
+            if let response = urlResponseOrNil as? HTTPURLResponse, response.statusCode == 200, let data = dataOrNil, let json = ((try? JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]) as [String : Any]??) {
+                completion(json, nil)
+            } else {
+                completion(nil, errorOrNil)
+            }
+        }
+        task.resume()
+    }
 }
