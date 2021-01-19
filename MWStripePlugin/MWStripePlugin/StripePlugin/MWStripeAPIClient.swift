cec9af92753ad38dd84005c6475aeb5f0851722a
Xavi Moll <xmollv@gmail.com>
2021-01-19 12:25:04 +0100

    Added an StripeAPIClient to encapsulate all the Stripe logic

---
+++ MWStripePlugin/MWStripePlugin/StripePlugin/MWStripeAPIClient.swift
@@ -0,0 +1,154 @@
+//
+//  MWStripeAPIClient.swift
+//  MWStripePlugin
+//
+//  Created by Xavi Moll on 19/1/21.
+//
+
+import Foundation
+import Stripe
+
+public enum PaymentContextResult {
+    case success
+    case error(error: Error)
+    case userCancelled
+}
+
+public protocol MWStripeAPIClientDelegate: class {
+    func paymentContextDidFailToLoad(withError error: Error)
+    func paymentContextDidChange(isLoading: Bool, selectedPaymentOptionLabel: String?)
+    func paymentContextDidFinishWith(result: PaymentContextResult)
+}
+
+public final class MWStripeAPIClient: NSObject {
+    
+    //MARK: Public properties
+    public weak var delegate: MWStripeAPIClientDelegate?
+    public var paymentContextHostViewController: UIViewController? {
+        set { self.paymentContext?.hostViewController = newValue }
+        get { self.paymentContext?.hostViewController }
+    }
+
+    //MARK: Private properties
+    private let step: MWStripeStep
+    private var customerContext: STPCustomerContext?
+    private var paymentContext: STPPaymentContext?
+    
+    //MARK: Lifecycle
+    public init(step: MWStripeStep) {
+        self.step = step
+        
+        // Step 1 - Provide a publishable key to set up the SDK: https://stripe.com/docs/mobile/ios/basic#setup-ios
+        StripeAPI.defaultPublishableKey = step.publishableKey
+        
+        super.init()
+        
+        // Step 3 - Set up an STPCustomerContext: https://stripe.com/docs/mobile/ios/basic#set-up-customer-context
+        self.customerContext = STPCustomerContext(keyProvider: self)
+        
+        // Step 4 - Set up an STPPaymentContext: https://stripe.com/docs/mobile/ios/basic#initialize-payment-context
+        self.paymentContext = STPPaymentContext(customerContext: self.customerContext!)
+        self.paymentContext?.delegate = self
+        self.paymentContext?.paymentAmount = 5000 // This is in cents, i.e. $50
+    }
+    
+    // Step 5 - Handle the user's payment method: https://stripe.com/docs/mobile/ios/basic#handle-payment-method
+    public func presentPaymentOptionsViewController() {
+        self.paymentContext?.presentPaymentOptionsViewController()
+    }
+    
+    // Step 6 - Submit the payment: https://stripe.com/docs/mobile/ios/basic#submit-payment
+    public func requestPayment() {
+        self.paymentContext?.requestPayment()
+    }
+}
+
+// Step 2 - Set up an ephemeral key: https://stripe.com/docs/mobile/ios/basic#ephemeral-key
+extension MWStripeAPIClient: STPCustomerEphemeralKeyProvider {
+    public func createCustomerKey(withAPIVersion apiVersion: String, completion: @escaping STPJSONResponseCompletionBlock) {
+        var urlComponents = URLComponents(url: self.step.ephemeralKeyURL, resolvingAgainstBaseURL: false)!
+        
+        urlComponents.queryItems = [URLQueryItem(name: "api_version", value: apiVersion)]
+        
+        //FIXME: Temporarly include a hardcoded email & customer ID
+        var extraQueryItems = urlComponents.queryItems
+        extraQueryItems?.append(URLQueryItem(name: "email", value: "matt@futureworkshops.com"))
+        extraQueryItems?.append(URLQueryItem(name: "customer_id", value: "cus_Il1PzN4kcyTooT"))
+        urlComponents.queryItems = extraQueryItems
+        
+        var request = URLRequest(url: urlComponents.url!)
+        request.httpMethod = "POST"
+        
+        let task = URLSession.shared.dataTask(with: request) { dataOrNil, urlResponseOrNil, errorOrNil in
+            if let response = urlResponseOrNil as? HTTPURLResponse, response.statusCode == 200, let data = dataOrNil, let json = ((try? JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]) as [String : Any]??) {
+                completion(json, nil)
+            } else {
+                completion(nil, errorOrNil)
+            }
+        }
+        task.resume()
+    }
+}
+
+extension MWStripeAPIClient: STPPaymentContextDelegate {
+    public func paymentContextDidChange(_ paymentContext: STPPaymentContext) {
+        self.delegate?.paymentContextDidChange(isLoading: paymentContext.loading, selectedPaymentOptionLabel: paymentContext.selectedPaymentOption?.label)
+    }
+    
+    public func paymentContext(_ paymentContext: STPPaymentContext, didCreatePaymentResult paymentResult: STPPaymentResult, completion: @escaping STPPaymentStatusBlock) {
+        
+        var components = URLComponents(url: self.step.paymentIntentURL, resolvingAgainstBaseURL: false)!
+        
+        //FIXME: This is temporary
+        components.queryItems = [
+            URLQueryItem(name: "email", value: "matt@futureworkshops.com"),
+            URLQueryItem(name: "customer_id", value: "cus_Il1PzN4kcyTooT"),
+            URLQueryItem(name: "product_id", value: "1")
+        ]
+        
+        var request = URLRequest(url: components.url!)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        let task = URLSession.shared.dataTask(with: request) { dataOrNil, urlResponseOrNil, errorOrNil in
+            if let response = urlResponseOrNil as? HTTPURLResponse, response.statusCode == 200, let data = dataOrNil, let json = ((try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]) as [String: Any]??), let secret = json?["secret"] as? String {
+                
+                let paymentIntentParams = STPPaymentIntentParams(clientSecret: secret)
+                paymentIntentParams.paymentMethodId = paymentResult.paymentMethod?.stripeId
+                
+                STPPaymentHandler.shared().confirmPayment(paymentIntentParams, with: paymentContext) { paymentStatus, paymentIntent, paymentError in
+                    switch paymentStatus {
+                    case .succeeded:
+                        completion(.success, nil)
+                    case .failed:
+                        completion(.error, paymentError)
+                    case .canceled:
+                        completion(.userCancellation, nil)
+                    @unknown default:
+                        assertionFailure("Unhandled case")
+                        completion(.error, nil)
+                    }
+                }
+                
+            } else {
+                completion(.error, errorOrNil)
+            }
+        }
+        task.resume()
+    }
+    
+    public func paymentContext(_ paymentContext: STPPaymentContext, didFinishWith status: STPPaymentStatus, error: Error?) {
+        switch status {
+        case .success:
+            self.delegate?.paymentContextDidFinishWith(result: .success)
+        case .error:
+            self.delegate?.paymentContextDidFinishWith(result: .error(error: error ?? NSError(domain: "io.mobileworkflow.stripe", code: 0, userInfo: [NSLocalizedDescriptionKey:"Payment context failed but the error was not present."])))
+        case .userCancellation:
+            self.delegate?.paymentContextDidFinishWith(result: .userCancelled)
+        }
+    }
+    
+    public func paymentContext(_ paymentContext: STPPaymentContext, didFailToLoadWithError error: Error) {
+        self.delegate?.paymentContextDidFailToLoad(withError: error)
+    }
+}
--- MWStripePlugin/MWStripePlugin/StripePlugin/MWStripeViewController.swift
+++ MWStripePlugin/MWStripePlugin/StripePlugin/MWStripeViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import Stripe
 import MobileWorkflowCore
 
 public class MWStripeViewController: ORKStepViewController {
@@ -16,184 +15,69 @@ public class MWStripeViewController: ORKStepViewController {
     
     //MARK: private properties
     private var stripeStep: MWStripeStep { self.step as! MWStripeStep }
-    private var customerContext: STPCustomerContext?
-    private var paymentContext: STPPaymentContext?
+    private var stripeAPIClient: MWStripeAPIClient! // Force-unwrapped because we need access to self
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.stripeAPIClient = MWStripeAPIClient(step: self.stripeStep)
+        self.stripeAPIClient.delegate = self
+        self.stripeAPIClient.paymentContextHostViewController = self
         
         self.configureBuyButtonToSelectPaymentOption()
         self.buyButton.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(self.buyButton)
         self.buyButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         self.buyButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-        
-        // Step 1 - Provide a publishable key to set up the SDK: https://stripe.com/docs/mobile/ios/basic#setup-ios
-        StripeAPI.defaultPublishableKey = self.stripeStep.publishableKey
-        
-        // Step 3 - Set up an STPCustomerContext: https://stripe.com/docs/mobile/ios/basic#set-up-customer-context
-        self.customerContext = STPCustomerContext(keyProvider: self)
-        
-        // Step 4 - Set up an STPPaymentContext: https://stripe.com/docs/mobile/ios/basic#initialize-payment-context
-        self.paymentContext = STPPaymentContext(customerContext: self.customerContext!)
-        self.paymentContext?.delegate = self
-        self.paymentContext?.hostViewController = self
-        self.paymentContext?.paymentAmount = 5000 // This is in cents, i.e. $50
     }
     
     private func configureBuyButtonToSelectPaymentOption() {
         self.buyButton.setTitle("Buy Item", for: .normal)
         self.buyButton.removeTarget(nil, action: nil, for: .allEvents)
-        self.buyButton.addTarget(self, action: #selector(buyButtonTappedToTriggerSelectionOfPaymentOption(_:)), for: .primaryActionTriggered)
+        self.buyButton.addTarget(self, action: #selector(presentPaymentOptions), for: .primaryActionTriggered)
     }
     
-    private func configureBuyButtonToBuy(usingPaymentOption paymentOption: STPPaymentOption) {
-        self.buyButton.setTitle("Buy Item using \(paymentOption.label)", for: .normal)
+    private func configureBuyButtonToBuy(usingSelectedPaymentOption paymentOptionLabel: String) {
+        self.buyButton.setTitle("Buy Item using \(paymentOptionLabel)", for: .normal)
         self.buyButton.removeTarget(nil, action: nil, for: .allEvents)
-        self.buyButton.addTarget(self, action: #selector(buyButtonTappedToConfirmPurchase(_:)), for: .primaryActionTriggered)
+        self.buyButton.addTarget(self, action: #selector(requestPayment), for: .primaryActionTriggered)
     }
     
-    //MARK: IBActions
-    @IBAction private func buyButtonTappedToTriggerSelectionOfPaymentOption(_ sender: UIButton) {
-        // Step 5 - Handle the user's payment method: https://stripe.com/docs/mobile/ios/basic#handle-payment-method
-        self.paymentContext?.presentPaymentOptionsViewController()
+    @objc private func presentPaymentOptions() {
+        self.stripeAPIClient.presentPaymentOptionsViewController()
     }
     
-    @IBAction private func buyButtonTappedToConfirmPurchase(_ sender: UIButton) {
-        // Step 6 - Submit the payment: https://stripe.com/docs/mobile/ios/basic#submit-payment
-        self.paymentContext?.requestPayment()
-    }
-    
-}
-
-// MARK: Stripe API
-
-// Step 2 - Set up an ephemeral key: https://stripe.com/docs/mobile/ios/basic#ephemeral-key
-extension MWStripeViewController: STPCustomerEphemeralKeyProvider {
-    public func createCustomerKey(withAPIVersion apiVersion: String, completion: @escaping STPJSONResponseCompletionBlock) {
-        guard var urlComponents = URLComponents(url: self.stripeStep.ephemeralKeyURL, resolvingAgainstBaseURL: false) else {
-            completion(nil, MWStripeError.failedToConstructEphemeralURL)
-            return
-        }
-        
-        urlComponents.queryItems = [URLQueryItem(name: "api_version", value: apiVersion)]
-        
-        //FIXME: Temporarly include a hardcoded email & customer ID
-        var extraQueryItems = urlComponents.queryItems
-        extraQueryItems?.append(URLQueryItem(name: "email", value: "matt@futureworkshops.com"))
-        extraQueryItems?.append(URLQueryItem(name: "customer_id", value: "cus_Il1PzN4kcyTooT"))
-        urlComponents.queryItems = extraQueryItems
-        
-        guard let finalURL = urlComponents.url else {
-            completion(nil, MWStripeError.failedToConstructEphemeralURL)
-            return
-        }
-        
-        var request = URLRequest(url: finalURL)
-        request.httpMethod = "POST"
-        
-        let task = URLSession.shared.dataTask(with: request) { dataOrNil, urlResponseOrNil, errorOrNil in
-            if let response = urlResponseOrNil as? HTTPURLResponse, response.statusCode == 200, let data = dataOrNil, let json = ((try? JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]) as [String : Any]??) {
-                completion(json, nil)
-            } else {
-                completion(nil, errorOrNil)
-            }
-        }
-        task.resume()
+    @objc private func requestPayment() {
+        self.stripeAPIClient.requestPayment()
     }
 }
 
-extension MWStripeViewController: STPPaymentContextDelegate {
-    public func paymentContextDidChange(_ paymentContext: STPPaymentContext) {
-        print("paymentContextDidChange, isLoading: \(paymentContext.loading)")
-        if let selectedPaymentOption = paymentContext.selectedPaymentOption {
-            self.configureBuyButtonToBuy(usingPaymentOption: selectedPaymentOption)
+extension MWStripeViewController: MWStripeAPIClientDelegate {
+    public func paymentContextDidFailToLoad(withError error: Error) {
+        let alertController = UIAlertController(title: "Failed to load", message: error.localizedDescription, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default) { [weak self] _ in
+            self?.goBackward()
+        })
+        self.present(alertController, animated: true)
+    }
+    
+    public func paymentContextDidChange(isLoading: Bool, selectedPaymentOptionLabel: String?) {
+        if let selectedPaymentOption = selectedPaymentOptionLabel {
+            self.configureBuyButtonToBuy(usingSelectedPaymentOption: selectedPaymentOption)
         } else {
             self.configureBuyButtonToSelectPaymentOption()
         }
     }
     
-    public func paymentContext(_ paymentContext: STPPaymentContext, didCreatePaymentResult paymentResult: STPPaymentResult, completion: @escaping STPPaymentStatusBlock) {
-        
-        guard var components = URLComponents(url: self.stripeStep.paymentIntentURL, resolvingAgainstBaseURL: false) else {
-            completion(.error, MWStripeError.failedToConstructPaymentIntentURL)
-            return
+    public func paymentContextDidFinishWith(result: PaymentContextResult) {
+        let message: String
+        switch result {
+        case .success: message = "Successfully purchased the product"
+        case .error(let error): message = error.localizedDescription
+        case .userCancelled: message = "User cancelled the purchase"
         }
-        
-        //FIXME: This is temporary
-        components.queryItems = [
-            URLQueryItem(name: "email", value: "matt@futureworkshops.com"),
-            URLQueryItem(name: "customer_id", value: "cus_Il1PzN4kcyTooT"),
-            URLQueryItem(name: "product_id", value: "1")
-        ]
-        
-        guard let finalURL = components.url else {
-            completion(.error, MWStripeError.failedToConstructPaymentIntentURL)
-            return
-        }
-        
-        var request = URLRequest(url: finalURL)
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpMethod = "POST"
-        
-        let task = URLSession.shared.dataTask(with: request) { dataOrNil, urlResponseOrNil, errorOrNil in
-            if let response = urlResponseOrNil as? HTTPURLResponse, response.statusCode == 200, let data = dataOrNil, let json = ((try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]) as [String: Any]??), let secret = json?["secret"] as? String {
-                
-                let paymentIntentParams = STPPaymentIntentParams(clientSecret: secret)
-                paymentIntentParams.paymentMethodId = paymentResult.paymentMethod?.stripeId
-                
-                STPPaymentHandler.shared().confirmPayment(paymentIntentParams, with: paymentContext) { paymentStatus, paymentIntent, paymentError in
-                    switch paymentStatus {
-                    case .succeeded:
-                        completion(.success, nil)
-                    case .failed:
-                        completion(.error, paymentError)
-                    case .canceled:
-                        completion(.userCancellation, nil)
-                    @unknown default:
-                        assertionFailure("Unhandled case")
-                        completion(.error, nil)
-                    }
-                }
-                
-            } else {
-                completion(.error, errorOrNil)
-            }
-        }
-        task.resume()
-        
-    }
-    
-    public func paymentContext(_ paymentContext: STPPaymentContext, didFinishWith status: STPPaymentStatus, error: Error?) {
-        switch status {
-        case .success:
-            let alertController = UIAlertController(title: "Success", message: "Successfully purchased the product.", preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
-            self.present(alertController, animated: true)
-        case .error:
-            let alertController = UIAlertController(title: "Error", message: error?.localizedDescription ?? "UNKNOWN ERROR", preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
-            self.present(alertController, animated: true)
-        case .userCancellation:
-            print("User cancelled the payment, do nothing")
-        }
-    }
-    
-    public func paymentContext(_ paymentContext: STPPaymentContext, didFailToLoadWithError error: Error) {
-        assertionFailure("Failed to load the Payment Context")
-        self.dismiss(animated: true)
-    }
-}
-
-// MARK: Errors
-enum MWStripeError: LocalizedError {
-    case failedToConstructEphemeralURL
-    case failedToConstructPaymentIntentURL
-    
-    var errorDescription: String? {
-        switch self {
-        case .failedToConstructEphemeralURL: return "Failed to construct the URL to retrieve the ephemeral key."
-        case .failedToConstructPaymentIntentURL: return "Failed to construct the URL to send the payment intent."
-        }
+        let alertController = UIAlertController(title: "Purchase Result", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+        self.present(alertController, animated: true)
     }
 }
